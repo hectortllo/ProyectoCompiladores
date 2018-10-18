ff7a53665d7e0bc4b32b982c3592a4eb1a5a4fce
Héctor Tello <hectortllo@gmail.com>
2018-10-18 12:18:40 -0600

    Cree el proyecto y la clase main

---
+++ ProyectoCompiladores/src/proyectocompiladores/ProyectoCompiladores.java
@@ -0,0 +1,12 @@
+package proyectocompiladores;
+
+/**
+ *
+ * @author Héctor Tello, hectortllo@gmail.com
+ */
+public class ProyectoCompiladores {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

235d8d82ac2e29f8e218eb9acd4c18dd497b86f8
Héctor Tello <hectortllo@gmail.com>
2018-11-13 11:21:44 -0600

    Se agregó la escritura del archivo, no sé si funciona

--- ProyectoCompiladores/src/Main/Principal.java
+++ ProyectoCompiladores/src/Main/Principal.java
@@ -7,9 +7,13 @@ package Main;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import proyectocompiladores.AnalisisSintactico;
+import proyectocompiladores.Archivo;
+import proyectocompiladores.ArrayListTokens;
 import proyectocompiladores.Lexico;
 
 /**
@@ -26,6 +30,14 @@ public class Principal {
             } catch (Exception ex) {
                 Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
             }
+            ArrayList<ArrayListTokens> tokens = Lexico.tokens;
+            for(int i = 0; i <=tokens.size()-1; i++){
+                System.out.print("Tipo de token: " + tokens.get(i).getTipo_token());
+                System.out.println(" - Nombre: " + tokens.get(i).getNombre());
+            }
+            Collections.sort(tokens);
+            Archivo miArchivo = new Archivo();
+            miArchivo.escribirArchivo(tokens);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
         }
